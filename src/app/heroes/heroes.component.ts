9446b5eb27ae31faea4668010e54c0995cb45fe6
hong-il <clroyan01@gmail.com>
2020-02-05 11:27:39 +0900

    Update service - observable hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,6 +24,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
