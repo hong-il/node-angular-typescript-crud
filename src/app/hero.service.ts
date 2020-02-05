7e001dc51dd1c5d8449e5abecd82d60b811a5b8f
hong-il <clroyan01@gmail.com>
2020-02-05 14:25:00 +0900

    Create service - message

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,14 +4,17 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
