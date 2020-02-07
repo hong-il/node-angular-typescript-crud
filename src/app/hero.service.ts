e7ce356e0493ca56eecc3b327fe9ed18a56945c0
hong-il <clroyan01@gmail.com>
2020-02-07 13:03:51 +0900

    Update hero service - handle error

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 
@@ -19,11 +20,28 @@ export class HeroService {
     ) { }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    return this.http.get<Hero>(this.heroesUrl + '/' + id);
+    return this.http.get<Hero>(this.heroesUrl + '/' + id)
+      .pipe(
+        catchError(this.handleError<Hero>('getHero'))
+      );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.log(error);
+      
+      this.log('${operation} failed: ' + error.message);
+
+      return of(result as T);
+    }
   }
 
   private log(message: string) {
